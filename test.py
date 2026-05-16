b0af7807f197c20dd3bda0b66ed3331e0c3c2202
lfj <juzitinghai@gmail.com>
2026-05-16 17:41:48 +0800

    test py file

---
+++ test.py
@@ -0,0 +1,12 @@
+
+import torch
+from torch import nn
+
+X = torch.tensor([[0.0, 1.0], [2.0, 3.0]])
+K = torch.tensor([[0.0, 1.0], [2.0, 3.0]])
+X, K = X.reshape(1, 1, 2, 2), K.reshape(1, 1, 2, 2)
+print(f'x',X)
+print(f'k',K)
+tconv = nn.ConvTranspose2d(1, 1, kernel_size=2, bias=False)
+tconv.weight.data = K
+print(f'tconv',tconv(X))
